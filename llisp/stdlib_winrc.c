58bbb9481ba0e95bd6e78d71aebcde4455f5b010
Louis Wilson <louis@louiswins.com>
2026-04-15 23:02:48 -0500

    Load stdlib from the embedded resource
    This is only implemented on Windows using the resource compiler, hence the name _winrc. We can do it with #embed or xxd or something else on other platforms. (And #embed on all platforms, once all compilers implement it.)

--- llisp/stdlib_winrc.c
+++ llisp/stdlib_winrc.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
+#define WIN32_LEAN_AND_MEAN
+#include "resource.h"
+#include <windows.h>
+
+// windows.h and myself both want to define these
+#undef TRUE
+#undef FALSE
+
 #include "cps.h"
 #include "parse.h"
 #include "stdlib.h"
 
 void add_stdlib(struct env *env) {
-	FILE *stdlib_fp = fopen("stdlib.llisp", "r");
-	if (!stdlib_fp) {
-		fputs("Warning: unable to locate standard library\n", stderr);
-		return;
-	}
+	HRSRC hResStdlib = FindResourceW(NULL, MAKEINTRESOURCEW(IDR_STDLIB), RT_RCDATA);
+	if (!hResStdlib) abort();
+	DWORD stdlibLen = SizeofResource(NULL, hResStdlib);
+	if (!stdlibLen) abort();
+	HGLOBAL hStdlib = LoadResource(NULL, hResStdlib);
+	if (!hStdlib) abort();
+	const char *stdlibData = LockResource(hStdlib);
+
 	struct data_source stdlib;
-	data_source_from_file(stdlib_fp, &stdlib);
+	data_source_from_memory(stdlibData, stdlibLen, &stdlib);
 
 	init_parser();
 	struct obj *obj;
 	while ((obj = parse(&stdlib)) != NULL) {
 		run_cps(obj, env);
 	}
-
-	fclose(stdlib_fp);
 }
